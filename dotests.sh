24f32c37c00ecd8c5f6547d1127dc1c0740f1d97
Davide Sandonà <sandona.davide@gmail.com>
2025-05-08 19:02:01 +0200

    regorganized test sequence

--- dotests.sh
+++ dotests.sh
@@ -2,7 +2,9 @@
 
 echo 'Core tests:'
 pytest --ignore='Developer Testing' --ignore-glob='*test_preparser.py' --ignore-glob='*test_numerics.py'
-echo 'Doc tests:'
-pytest --ignore='tests' --ignore='Developer Testing' --ignore-glob='*old*'  --doctest-modules
+
 echo 'Preparser and numerics tests (require ipython environment):'
 ipython -m pytest tests/test_preparser.py tests/test_numerics.py
+
+echo 'Doc tests:'
+pytest --ignore='tests' --ignore='Developer Testing' --ignore-glob='*old*'  --doctest-modules
