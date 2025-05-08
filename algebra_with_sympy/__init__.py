cccecae7ff38d61cb03e7164a36059cef454920b
Davide Sandonà <sandona.davide@gmail.com>
2025-05-08 19:01:27 +0200

    improved import statements

--- algebra_with_sympy/__init__.py
+++ algebra_with_sympy/__init__.py
@@ -1,23 +1,21 @@
-"""
-.. include:: ../ReadMe.md
-.. include:: ../Development Notes.md
-"""
-__docformat__ = "numpy"
 from algebra_with_sympy.algebraic_equation import (
     algwsym_config,
     Equation,
     Eqn,
+    Equality,
+    Eq,
     solve,
     solveset,
 )
 
-from algebra_with_sympy.version import __version__
-algwsym_version = __version__
+from algebra_with_sympy._version import __version__
 
 __all__ = [
     "algwsym_config",
     "Equation",
     "Eqn",
+    "Equality",
+    "Eq",
     "solve",
     "solveset",
 ]
