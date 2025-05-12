4ea55d9e4b40668a29f5766a2fb46e2ed1a7329b
Davide Sandonà <sandona.davide@gmail.com>
2025-05-12 09:39:05 +0200

    updated tests executions

--- dotests.sh
+++ dotests.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 echo 'Core tests:'
-pytest --ignore='Developer Testing' --ignore-glob='*test_preparser.py' --ignore-glob='*test_numerics.py'
+pdm run pytest --ignore='Developer Testing' --ignore-glob='*test_preparser.py' --ignore-glob='*test_numerics.py'
 
 echo 'Preparser and numerics tests (require ipython environment):'
-ipython -m pytest tests/test_preparser.py tests/test_numerics.py
+pdm run ipython -m pytest tests/test_preparser.py tests/test_numerics.py
 
 echo 'Doc tests:'
-pytest --ignore='tests' --ignore='Developer Testing' --ignore-glob='*old*'  --doctest-modules
+pdm run pytest --ignore='tests' --ignore='Developer Testing' --ignore-glob='*old*'  --doctest-modules
