936f77b7780a428e9d83d035291caf46d9bd7814
Davide Sandonà <sandona.davide@gmail.com>
2025-05-18 08:32:23 +0200

    fixed typo

--- tests/test_algebraic_equation.py
+++ tests/test_algebraic_equation.py
@@ -449,4 +449,4 @@ def test_version():
 
 @pytest.mark.parametrize("eq", [Eqn(a, b), Eqn(a + b, c/d)])
 def test_negation(eq):
-    assert -neg == Equation(-e.lhs, -e.rhs)
+    assert -neg == Equation(-eq.lhs, -eq.rhs)
