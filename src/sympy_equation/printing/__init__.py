5bfe35449a680ea87be2d81d9a8b617ad8113bdd
Davide Sandonà <sandona.davide@gmail.com>
2026-02-02 16:22:11 +0100

    fix import

---
+++ src/sympy_equation/printing/__init__.py
@@ -0,0 +1,9 @@
+from sympy_equation.printing.extended_latex import extended_latex
+from sympy_equation.printing.multiline_latex import multiline_latex
+from sympy_equation.printing.interactive import init_latex_printing
+
+__all__ = [
+    "extended_latex",
+    "multiline_latex",
+    "init_latex_printing"
+]
